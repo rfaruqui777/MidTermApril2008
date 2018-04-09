d7d6e96cdb7c09ebda3e7398d8e763ca7b369c1f
rfaruqui777 <faruqui.ruksana777@gmail.com>
2018-04-08 17:43:32 -0700

    UseMap

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,4 +1,7 @@
 package datastructure;
+import databases.ConnectDB;
+
+import java.util.*;
 
 public class UseMap {
 
@@ -11,6 +14,24 @@ public class UseMap {
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
 
+		List<String> listBookScience = new ArrayList<String> ();
+		listBookScience.add("Math");
+		listBookScience.add("Science");
+		listBookScience.add("Physics");
+		listBookScience.add("Chemistry");
+		List<String> listBookGeneral = new ArrayList<String> ();
+		listBookGeneral.add("English");
+		listBookGeneral.add("ICT");
+		listBookGeneral.add("Bengali");
+		Map<String, List<String>> map = new LinkedHashMap<String, List<String>>();
+		map.put("Science", listBookScience);
+		map.put("General", listBookGeneral);
+		for (Map.Entry entry:map.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+			ConnectDB db = new ConnectDB();
+			db.InsertDataFromArrayListToMySql(Collections.singletonList(map),"BookList", "Book_Name");
+
+		}
 	}
 
-}
