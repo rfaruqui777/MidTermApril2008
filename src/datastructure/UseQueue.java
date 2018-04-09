e74e9b0ae5411daa0f01fdeaa9a6515492170b36
rfaruqui777 <faruqui.ruksana777@gmail.com>
2018-04-08 17:54:57 -0700

    DataStructureFinished

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,7 +12,17 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+        Queue<String> queue = new LinkedList<>();
+        queue.add("Afsana");
+        queue.add("Afroza");
+        queue.add("Sabrina");
+        for (String it:queue)
+            System.out.println(it);
 
+        Iterator it = queue.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
 	}
 
 }
