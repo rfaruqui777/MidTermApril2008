ab559f24ddf63a5eb68d4c5bacc95443fda23e24
rfaruqui777 <faruqui.ruksana777@gmail.com>
2018-04-09 05:31:28 -0700

    Math

--- src/math/LowestNumber.java
+++ src/math/LowestNumber.java
@@ -15,8 +15,14 @@ public class LowestNumber {
 		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
 
 		//find lowest number from the array
-
-		ConnectDB connectDB = new ConnectDB();
+        int minValue = array[0];
+        for(int i=1;i<array.length;i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        System.out.println("The lowest value is:" + minValue);
+        ConnectDB connectDB = new ConnectDB();
 		List<String> lowestValue = new ArrayList<String>();
 		try {
             connectDB.InsertDataFromArryToMySql(array, "selection_sort", "SortingNumbers");
@@ -25,7 +31,7 @@ public class LowestNumber {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
+		System.out.println("Data is reading from the Table (selection_sort) and displaying to the console");
 		for(String st:lowestValue){
 			System.out.println(st);
 		}
